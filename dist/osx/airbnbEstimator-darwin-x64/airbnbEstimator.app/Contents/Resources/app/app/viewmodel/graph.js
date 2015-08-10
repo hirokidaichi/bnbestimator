47c3b89a989763a3d74ae2837599ccbf26f551a3
Hiroki Daichi <hirokidaichi@gmail.com>
2015-08-10 23:07:11 +0900

    build

--- dist/osx/airbnbEstimator-darwin-x64/airbnbEstimator.app/Contents/Resources/app/app/viewmodel/graph.js
+++ dist/osx/airbnbEstimator-darwin-x64/airbnbEstimator.app/Contents/Resources/app/app/viewmodel/graph.js
@@ -1,9 +1,48 @@
 var flumine = require("flumine");
 var Vue = require("vue");
 var sprintf = require("sprintf");
+var merge = require("merge");
 var Chart = require("bower_components/Chart.js/Chart.min.js");
+var clipboard = require("clipboard");
+var nativeImage = require("native-image");
+var DATA_OPTION = {
+    fillColor: "rgba(151,187,205,0.2)",
+    strokeColor: "rgba(151,187,205,1)",
+    pointColor: "rgba(151,187,205,1)",
+    pointStrokeColor: "#fff",
+    pointHighlightFill: "#fff",
+    pointHighlightStroke: "rgba(151,187,205,1)",
+};
 
+var GRAPH_OPTION = {
+    //String - 目盛りの線の色 
+    scaleLineColor: "rgba(0,0,0,.1)",
+    scaleLineWidth: 10,
+    //Boolean - 目盛りを表示するかどうか  
+    scaleShowLabels: true,
+    //String - 目盛りのフォント
+    scaleFontFamily: "'Arial'",
+    //Number - 目盛りのフォントサイズ 
+    scaleFontSize: 10,
+    //String - 目盛りのフォントスタイル bold→太字  
+    scaleFontStyle: "normal",
+    //String - 目盛りのフォント 
+    scaleFontColor: "#666",
+    ///Boolean - チャートの背景にグリッドを描画するか
+    scaleShowGridLines: true,
+    //String - チャート背景のグリッド色
+    scaleGridLineColor: "rgba(0,0,0,.05)",
+    //Number - チャート背景のグリッドの太さ
+    scaleGridLineWidth: 1,
+};
 
+var canvasToClipboard = function(c) {
+    return function() {
+        var image = nativeImage.createFromDataUrl(c.toDataURL());
+        clipboard.writeImage(image);
+        alert("クリップボードにグラフを保存しました。");
+    };
+};
 var graph = module.exports = function(selector) {
     var priceFormat = function(min, max) {
         if (min && max) {
@@ -48,18 +87,38 @@ var graph = module.exports = function(selector) {
                 var that = this;
                 return flumine.through(function() {
                     var dataset = that.dataset;
-                    var area = $(that.$el).find("#graphArea").get(0);
-                    area.innerHTML = "";
-                    var c1 = document.createElement("canvas");
-                    var c2 = document.createElement("canvas");
-                    c1.width = 160;
-                    c1.height = 90;
-                    c2.width = 160;
-                    c2.height = 90;
-                    var g1 = c1.getContext('2d');
-                    var g2 = c2.getContext('2d');
-                    area.appendChild(c1);
-                    area.appendChild(c2);
+                    var area = $("#graphArea");
+                    area.html("");
+                    var c1 = $("<canvas>", {
+                        width: 160,
+                        height: 90
+                    });
+                    var b1 = $("<button>", {
+                        addClass: "btn-xs btn-info pull-right",
+                        text: "copy"
+                    });
+                    var c2 = $("<canvas>", {
+                        width: 160,
+                        height: 90
+                    });
+                    var b2 = $("<button>", {
+                        addClass: "btn-xs btn-info pull-right",
+                        text: "copy"
+                    });
+
+                    var g1 = c1.get(0).getContext('2d');
+                    var g2 = c2.get(0).getContext('2d');
+
+                    b1.on("click",
+                        canvasToClipboard(c1.get(0)));
+
+                    b2.on("click",
+                        canvasToClipboard(c2.get(0)));
+
+                    area.append(c1);
+                    area.append(b1);
+                    area.append(c2);
+                    area.append(b2);
                     var labels = dataset.map(function(e) {
                         return e[0];
                     });
@@ -77,32 +136,20 @@ var graph = module.exports = function(selector) {
                     var scaleSteps = (scaleMax - scaleMin) / 1000;
                     var graphData = {
                         labels: labels,
-                        datasets: [{
+                        datasets: [merge({
                             label: "ああ",
-                            fillColor: "rgba(151,187,205,0.2)",
-                            strokeColor: "rgba(151,187,205,1)",
-                            pointColor: "rgba(151,187,205,1)",
-                            pointStrokeColor: "#fff",
-                            pointHighlightFill: "#fff",
-                            pointHighlightStroke: "rgba(151,187,205,1)",
                             data: data
-                        }]
+                        }, DATA_OPTION)]
                     };
                     var priceData = {
                         labels: labels,
-                        datasets: [{
+                        datasets: [merge({
                             label: "ああ",
-                            fillColor: "rgba(151,187,205,0.2)",
-                            strokeColor: "rgba(151,187,205,1)",
-                            pointColor: "rgba(151,187,205,1)",
-                            pointStrokeColor: "#fff",
-                            pointHighlightFill: "#fff",
-                            pointHighlightStroke: "rgba(151,187,205,1)",
                             data: prices
-                        }]
+                        }, DATA_OPTION)]
                     };
 
-                    new Chart(g1).Line(graphData, {
+                    new Chart(g1).Line(graphData, merge({
                         responsive: true,
                         scaleOverride: true,
                         //Number - 目盛りの間隔
@@ -111,29 +158,10 @@ var graph = module.exports = function(selector) {
                         scaleStepWidth: 10,
                         //Number - 目盛りの最小値
                         scaleStartValue: 0,
-                        //String - 目盛りの線の色 
-                        scaleLineColor: "rgba(0,0,0,.1)",
-                        //Number - 目盛りの線の幅  
-                        scaleLineWidth: 10,
-                        //Boolean - 目盛りを表示するかどうか  
-                        scaleShowLabels: true,
-                        //String - 目盛りのフォント
-                        scaleFontFamily: "'Arial'",
-                        //Number - 目盛りのフォントサイズ 
-                        scaleFontSize: 10,
-                        //String - 目盛りのフォントスタイル bold→太字  
-                        scaleFontStyle: "normal",
-                        //String - 目盛りのフォント 
-                        scaleFontColor: "#666",
-                        ///Boolean - チャートの背景にグリッドを描画するか
-                        scaleShowGridLines: true,
-                        //String - チャート背景のグリッド色
-                        scaleGridLineColor: "rgba(0,0,0,.05)",
-                        //Number - チャート背景のグリッドの太さ
-                        scaleGridLineWidth: 1,
                         scaleLabel: "<%=value %>%"
-                    });
-                    new Chart(g2).Line(priceData, {
+                    }, GRAPH_OPTION));
+
+                    new Chart(g2).Line(priceData, merge({
                         responsive: true,
                         scaleOverride: true,
                         //Number - 目盛りの間隔
@@ -142,27 +170,8 @@ var graph = module.exports = function(selector) {
                         scaleStepWidth: 1000,
                         //Number - 目盛りの最小値
                         scaleStartValue: scaleMin,
-                        //String - 目盛りの線の色 
-                        scaleLineColor: "rgba(0,0,0,.1)",
-                        scaleLineWidth: 10,
-                        //Boolean - 目盛りを表示するかどうか  
-                        scaleShowLabels: true,
-                        //String - 目盛りのフォント
-                        scaleFontFamily: "'Arial'",
-                        //Number - 目盛りのフォントサイズ 
-                        scaleFontSize: 10,
-                        //String - 目盛りのフォントスタイル bold→太字  
-                        scaleFontStyle: "normal",
-                        //String - 目盛りのフォント 
-                        scaleFontColor: "#666",
-                        ///Boolean - チャートの背景にグリッドを描画するか
-                        scaleShowGridLines: true,
-                        //String - チャート背景のグリッド色
-                        scaleGridLineColor: "rgba(0,0,0,.05)",
-                        //Number - チャート背景のグリッドの太さ
-                        scaleGridLineWidth: 1,
                         scaleLabel: "<%=value %>円"
-                    });
+                    }, GRAPH_OPTION));
 
                 });
             }
